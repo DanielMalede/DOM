7ef61f737a6cac9c83037666066960ecb98c77fd
DanielMalede <danielmalede@gmail.com>
2022-05-26 17:57:20 +0300

    DOM

---
+++ main.js
@@ -0,0 +1,23 @@
+// console.log(h1_first); 
+// document.getElementById("h2_first").innerText ="my dom app"
+// console.log(document.getElementsByClassName("h3_first"));
+// console.log(document.getElementsByClassName("h3_first")[1]);
+// console.log(document.getElementsByClassName("h3_first")[2].innerText);
+// console.log(document.getElementsByTagName("span"));
+// function printUsername() {
+//     var userName = prompt("enter your name")
+//     document.write("<h2>" + userName + "</h2>")
+// }
+// printUsername()
+// function printUserColor() {
+//     var userColor = prompt("enter your color")
+//     var userNumber = prompt("enter your number")
+//     for (var i = 0; i < userNumber; i++) {
+//         document.write("<span>" +" " + userColor + " " + "</span>")   
+//     }
+// }
+// printUserColor()
+
+function userNamePlu(params) {
+    
+}
